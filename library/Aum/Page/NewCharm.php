23a971896d3ccc3d741bac9f2f332a419d9eb6c2
zirk <dirk@iMac.local>
2010-08-04 00:15:18 +0200

    new charms :x

---
+++ library/Aum/Page/NewCharm.php
@@ -0,0 +1,22 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Aum_Page_NewCharm
+ *
+ * @author dirk
+ */
+class Aum_Page_NewCharm  extends Aum_Page_Abstract{
+    
+    function __construct() {
+
+    }
+
+    public function setHtmlBody($htmlBody){
+        parent::setHtmlBody('<?xml version="1.0" encoding="iso-8859-15"?>' . html_entity_decode($htmlBody));
+    }
+}
+?>
